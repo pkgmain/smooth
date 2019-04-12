9415f32f389266f0380df15030f2477bd234bb08
Shaunn Barron <shaunn.barron@chase.com>
2019-04-12 16:31:14 -0400

    Migration helpers

--- pop.go
+++ pop.go
@@ -7,6 +7,48 @@ import (
 	"github.com/pkg/errors"
 )
 
+type PopMigrator struct {
+	box pop.MigrationBox
+}
+
+func NewMigrator(conn *pop.Connection, fs http.FileSystem) (m PopMigrator, _ error) {
+	walkable := NewPackdWalkable(fs)
+	box, err := pop.NewMigrationBox(walkable, conn)
+	if err != nil {
+		return m, err
+	}
+
+	return PopMigrator{box}, nil
+}
+
+func (m PopMigrator) Up() error {
+	err := m.box.Up()
+	if err != nil {
+		return err
+	}
+
+	err = m.box.Status()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m PopMigrator) Down() error {
+	err := m.box.Down(1)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	err = m.box.Status()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func PopConnect(fs http.FileSystem, env string) (*pop.Connection, error) {
 	cfg, err := fs.Open("database.yml")
 	if err != nil {
