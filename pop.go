f9d02327ac55da0b501940c5a25c9856591fb3b3
Shaunn Barron <shaunn.barron@chase.com>
2019-04-12 16:26:05 -0400

    Pop connection helper

--- pop.go
+++ pop.go
@@ -2,57 +2,21 @@ package smooth
 
 import (
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/gobuffalo/packd"
+	"github.com/gobuffalo/pop"
 	"github.com/pkg/errors"
-	"github.com/shurcooL/httpfs/vfsutil"
 )
 
-type PackdWalkable struct {
-	fs http.FileSystem
-}
+func PopConnect(fs http.FileSystem, env string) (*pop.Connection, error) {
+	cfg, err := fs.Open("database.yml")
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
 
-func NewPackdWalkable(fs http.FileSystem) *PackdWalkable {
-	return &PackdWalkable{fs: fs}
-}
+	err = pop.LoadFrom(cfg)
+	if err != nil {
+		return nil, err
+	}
 
-func (p PackdWalkable) Walk(wf packd.WalkFunc) error {
-	return vfsutil.Walk(p.fs, "/", func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-
-		f, err := p.fs.Open(path)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-
-		file, err := packd.NewFile(info.Name(), f)
-		if err != nil {
-			return err
-		}
-
-		err = wf(path, file)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-
-		return nil
-	})
-}
-
-func (p PackdWalkable) WalkPrefix(prefix string, wf packd.WalkFunc) error {
-	return p.Walk(func(path string, file packd.File) error {
-		if strings.HasPrefix(filepath.ToSlash(path), filepath.ToSlash(prefix)) {
-			err := wf(path, file)
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
+	return pop.Connect(env)
 }
